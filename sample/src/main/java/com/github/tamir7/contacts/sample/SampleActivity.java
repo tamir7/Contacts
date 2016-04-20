c3b9cc020f609757b5a81e043d2c195d638517ad
Tamir Shomer <tamir7@gmail.com>
2016-04-20 17:22:12 +0300

    Empty project

---
+++ sample/src/main/java/com/github/tamir7/contacts/sample/SampleActivity.java
@@ -0,0 +1,4 @@
+package com.github.tamir7.contacts.sample;
+
+public class SampleActivity {
+}
---
+++ sample/src/main/java/com/github/tamir7/contacts/sample/SampleApplication.java
@@ -0,0 +1,7 @@
+package com.github.tamir7.contacts.sample;
+
+/**
+ * Created by tamir on 20/04/16.
+ */
+public class SampleApplication {
+}
